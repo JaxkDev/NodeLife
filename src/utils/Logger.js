987c8ff3f5ebebadbcb7c35e51e7ceaa8518dc8a
Jack Honour <gangnam253@gmail.com>
2018-10-15 14:36:01 +0100

    start logger implement

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -19,31 +19,40 @@ class Logger{
     }
 
     debug(msg){
-
+        this._logMsg(0, msg);
     }
 
     log(msg){
-
+        this._logMsg(1, msg);
     }
 
     info(msg){
-
+        this._logMsg(2, msg);
     }
 
     warn(msg){
-
+        this._logMsg(3, msg);
     }
 
     error(msg){
-
+        this._logMsg(4, msg);
     }
 
     critical(msg){
-
+        this._logMsg(5, msg);
     }
 
     _logMsg(level, message){
-
+        if((level == 2 || level == 0) && this.cfg.debug){
+            process.stdout.write(this._format(level, message, this.cfg.colour));
+        }
+        if(level != 2 && level != 0){
+            process.stdout.write(this._format(level, message, this.cfg.colour));
+        }
+    }
+                                     
+    _format(level, msg, colour){
+        
     }
 }
 
