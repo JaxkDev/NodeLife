2885dfbff8df922efdac1d10547969f6511c0aef
Jackthehaxk21 <gangnam253@gmail.com>
2018-10-15 17:26:40 +0100

    Happy yet ?

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -17,16 +17,12 @@ const colours = require('./Colour.js');
 
 const getTime = function() {
     var date = new Date();
-
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
-
     var min  = date.getMinutes();
     min = (min < 10 ? "0" : "") + min;
-
     var sec  = date.getSeconds();
     sec = (sec < 10 ? "0" : "") + sec;
-
     return hour + ":" + min + ":" + sec;
 }
 
