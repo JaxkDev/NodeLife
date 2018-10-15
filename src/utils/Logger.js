cb6b8fdf7c5cde0aa134eba890a2dec5eb1177eb
Jackthehaxk21 <gangnam253@gmail.com>
2018-10-15 17:18:48 +0100

    Update logger and add color file

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -13,6 +13,25 @@
 *
 */
 
+const colours = require('./Colour.js');
+
+const getTime = function() {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    return hour + ":" + min + ":" + sec;
+}
+
+const levels = ['debug','info','warn','error','critical'];
+
 class Logger{
     constructor(data){
         this.cfg = data.getConfig();
@@ -22,37 +41,46 @@ class Logger{
         this._logMsg(0, msg);
     }
 
-    log(msg){
+    info(msg){
         this._logMsg(1, msg);
     }
 
-    info(msg){
+    warn(msg){
         this._logMsg(2, msg);
     }
 
-    warn(msg){
+    error(msg){
         this._logMsg(3, msg);
     }
 
-    error(msg){
+    critical(msg){
         this._logMsg(4, msg);
     }
 
-    critical(msg){
-        this._logMsg(5, msg);
-    }
-
     _logMsg(level, message){
-        if((level == 2 || level == 0) && this.cfg.debug){
+        if(level <= 1 && this.cfg.debug){
             process.stdout.write(this._format(level, message, this.cfg.colour));
         }
-        if(level != 2 && level != 0){
+        if(level > 1){
             process.stdout.write(this._format(level, message, this.cfg.colour));
         }
     }
                                      
     _format(level, msg, colour){
-        
+        let colour = ''
+        switch(level){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+        return this.cfg.log_format.replace('{msg}',msg).replace('{level}', levels[level]).replace('{time}', getTime());
     }
 }
 
