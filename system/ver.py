60f0c260adc5e1e9cf6ccc34ce52e31146f78f95
Jack Honour <gangnam253@gmail.com>
2018-11-23 07:37:25 +0000

    Release 0.0.4

--- system/ver.py
+++ system/ver.py
@@ -26,7 +26,7 @@
 import sys,os
 
 def http():
-    return '004_dev' #_rel/_dev
+    return '004_rel' #_rel/_dev
 
 def ver():
     return '0.0.4'
@@ -38,7 +38,7 @@ def codeName():
     return 'Charlie' #Phonetic Alphabet
 
 def release():
-    return False
+    return True
 
 def bundled():
     if (getattr(sys, 'frozen', False) and (sys.argv[0] == sys.executable)):
